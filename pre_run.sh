cb5110793d48f485612bce9466e5db365ffc0623
Barack K <barackk321@gmail.com>
2022-02-16 18:09:31 +0200

    change file name

---
+++ pre_run.sh
@@ -0,0 +1,10 @@
+# on host pc
+
+# Create new ssh key
+# ssh-keygen -o -a 100 -t ed25519 -f ~/.ssh/serv -C barak7008@gmail.com 
+
+ssh-copy-id -i ~/.ssh/serv serv@192.168.1.119 # copying ssh key
+ansible -all -m ping # check if ssh works
+ansible-playbook run.yml -K - enter password
+# Now it will disable password ssh login, to run
+# ansible-playbook run.yml
